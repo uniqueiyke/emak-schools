7c2fcfe50bb76ecef78e50ec064383eaf31fdce0
Ikechukwu Ajike <iykeisunique@gmail.com>
2021-08-13 13:19:30 +0100

    index.js file removed from the server

--- server/app.js
+++ server/app.js
@@ -30,7 +30,6 @@ app.use(cookieParser());
 app.use(cors({origin: process.env.origin}));
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
 app.use('/staffs', require('./routes/staffs'));
 app.use('/students', require('./routes/students'));
 
