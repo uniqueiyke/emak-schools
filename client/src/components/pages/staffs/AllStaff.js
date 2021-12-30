9852ffd49b596edd6d655c2e8a6820de14871fcf
Ikechukwu Ajike <iykeisunique@gmail.com>
2021-12-30 05:25:50 +0100

    allStaff component updated

--- client/src/components/pages/staffs/AllStaff.js
+++ client/src/components/pages/staffs/AllStaff.js
@@ -1,23 +1,30 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { useSelector, useDispatch } from 'react-redux';
 import Staff from './Staff';
 import { fetchAllStaffs } from '../../../redux/actions/admin-action';
-
+import DataFetchingProgress from '../../other-components/DataFetchingProgress';
+import Errors from '../../other-components/Errors';
 
 
 const AllStaff = () => {
     const dispatch = useDispatch();
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(fetchAllStaffs())
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    
-    const { data } = useSelector(state => state.admin.staffs);
+
+    const { data, error, isFetchingAllStaffs } = useSelector(state => state.admin.staffs);
     return (
         <>
+            {
+                error && <Errors errors={error} goBack />
+            }
+            {
+                isFetchingAllStaffs && <DataFetchingProgress />
+            }
             {
                 data && <List>
                     {
